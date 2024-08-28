d05699fd23eb73b1fb61d44bebb12a9fd7c5e08b
Arrasyid Fadel Fatonsyah <aff.anton20@gmail.com>
2024-08-28 22:18:58 +0800

    update css

--- src/pages/aghniyaIzzul/AghniyaIzzul.tsx
+++ src/pages/aghniyaIzzul/AghniyaIzzul.tsx
@@ -4,59 +4,29 @@ import flower from '@assets/images/flower.png'
 import rose from '@assets/images/rose.png'
 import bride from '@assets/images/bride.png'
 import brideMen from '@assets/images/bride-men.png'
-import { useEffect, useRef } from 'react'
 
 function AghniyaIzzul() {
-  const sectionRefs = useRef<HTMLElement[]>([]);
-
-  useEffect(() => {
-    const handleScroll = (event: WheelEvent) => {
-      event.preventDefault();
-
-      // Mendapatkan index dari section saat ini
-      const currentSectionIndex = Math.round(window.scrollY / window.innerHeight);
-      
-      // Menentukan arah scroll (ke bawah atau ke atas)
-      const direction = event.deltaY > 0 ? 1 : -1;
-      const newSectionIndex = currentSectionIndex + direction;
-
-      // Pastikan index baru dalam rentang yang valid
-      if (newSectionIndex >= 0 && newSectionIndex < sectionRefs.current.length) {
-        sectionRefs.current[newSectionIndex].scrollIntoView({ behavior: 'smooth' });
-      }
-    };
-
-    // Menambahkan event listener untuk wheel (scroll) event
-    window.addEventListener('wheel', handleScroll, { passive: false });
-
-    // Membersihkan event listener saat komponen unmount
-    return () => {
-      window.removeEventListener('wheel', handleScroll);
-    };
-  }, []);
   return (
     <div className='agniya__container'>
-      <section ref={(el) => (sectionRefs.current[0] = el!)} className='aghniya__cover'>
-        <div className='aghniya__cover-image fade-in-top'>
+      <section className='aghniya__cover'>
+        <div className='aghniya__cover-image'>
           <img src={cover} alt="cover" />
         </div>
         <div className='aghniya__cover-title'>
-          <img className='element-img fade-in-left' src={flower} alt="flower" />
-          <img className='element-img fade-in-right' src={flower} alt="flower" />
+          <img className='element-img' src={flower} alt="flower" />
+          <img className='element-img' src={flower} alt="flower" />
 
-          <div className='aghniya__cover-partner fade-in-top'>
+          <div className='aghniya__cover-partner'>
             <h1>AGHNIYA</h1>
             <span className='comforter-brush-regular'>dan</span>
             <h1>IZZUL</h1>
             <img className="rose-element rotate-center" src={rose} alt="rose" />
           </div>
-
-          <h1 className='questrial-regular fade-in-top'>24.12.2024</h1>
         </div>
       </section>
 
-      <section ref={(el) => (sectionRefs.current[1] = el!)} className='aghniya__opening'>
-        <p className='fade-in-top'>
+      <section className='aghniya__opening'>
+        <p>
           “Dan di antara tanda-tanda (kebesaran)-Nya ialah Dia menciptakan
           pasangan-pasangan untukmu dari jenis kamu sendiri, supaya kamu
           cenderung dan merasa tenteram kepadanya, dan Dia menjadikan
@@ -70,7 +40,7 @@ function AghniyaIzzul() {
         <img className='elemet-rose rotate-center' src={rose} alt="rose" />
       </section>
 
-      <section ref={(el) => (sectionRefs.current[2] = el!)} className='aghniya__bride'>
+      <section className='aghniya__bride'>
         <div className='aghniya__top-rose'>
           <img className='rotate-center' src={rose} alt="rose" />
           <img className='rotate-center' src={rose} alt="rose" />
@@ -93,7 +63,7 @@ function AghniyaIzzul() {
         </p>
       </section>
 
-      <section ref={(el) => (sectionRefs.current[3] = el!)} className='aghniya__bride'>
+      <section className='aghniya__bride'>
         <div className='aghniya__top-rose'>
           <img className='rotate-center' src={rose} alt="rose" />
           <img className='rotate-center' src={rose} alt="rose" />
