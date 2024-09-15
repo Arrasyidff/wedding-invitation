7874670b364c38d3dce75220675e6eceeefd7e06
Arrasyid Fadel Fatonsyah <aff.anton20@gmail.com>
2024-09-15 15:23:59 +0800

    feat: update ui

--- src/pages/aghniyaIzzul/AghniyaIzzul.tsx
+++ src/pages/aghniyaIzzul/AghniyaIzzul.tsx
@@ -1,28 +1,10 @@
 import './aghniyaIzzul.scss'
+import {Cover} from './components'
 
 function AghniyaIzzul() {
   return (
     <div className='agniya__container'>
-      <section className='aghniya__cover'>
-        <div className="aghniya__cover-header">
-          <h2>SAVE THE DATE</h2>
-          <p>FOR THE WEDDING OF</p>
-        </div>
-
-        <div className='aghniya__cover-content'>
-        </div>
-
-        <div className='aghniya__cover-invitation'>
-          <span>KEPADA YTH.</span>
-          <span>BAPAK/IBU/SAUDARA/I</span>
-          <h1>BPK. ANDRE</h1>
-          <span>DI TEMPAT</span>
-        </div>
-
-        <button className='agniya__cover-open'>
-          BUKA UNDANGAN
-        </button>
-      </section>
+      <Cover />
     </div>
   )
 }
---
+++ src/pages/aghniyaIzzul/components/cover/Cover.tsx
@@ -0,0 +1,28 @@
+import './cover.scss'
+
+function Cover() {
+  return (
+    <section className='aghniya__cover'>
+        <div className="aghniya__cover-header">
+            <h2>SAVE THE DATE</h2>
+            <p>FOR THE WEDDING OF</p>
+        </div>
+
+        <div className='aghniya__cover-content'>
+        </div>
+
+        <div className='aghniya__cover-invitation'>
+            <span>KEPADA YTH.</span>
+            <span>BAPAK/IBU/SAUDARA/I</span>
+            <h1>BPK. ANDRE</h1>
+            <span>DI TEMPAT</span>
+        </div>
+
+        <button className='agniya__cover-open'>
+            BUKA UNDANGAN
+        </button>
+    </section>
+  )
+}
+
+export default Cover
