2eeaeb6a57ed4533eb9b9243e376961b1c1ffa98
Arrasyid Fadel Fatonsyah <aff.anton20@gmail.com>
2024-09-15 16:34:48 +0800

    feat: update ui

--- src/pages/aghniyaIzzul/components/cover/Cover.tsx
+++ src/pages/aghniyaIzzul/components/cover/Cover.tsx
@@ -5,7 +5,6 @@ function Cover() {
     <section className='aghniya__cover'>
         <div className="aghniya__cover-header">
             <h2>SAVE THE DATE</h2>
-            {window.innerWidth} x {window.innerHeight}
             <p>FOR THE WEDDING OF</p>
         </div>
 
