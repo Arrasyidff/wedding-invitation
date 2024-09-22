0d1f5d15f8976461fc3e93f57504a17f9218e36b
Arrasyid Fadel Fatonsyah <aff.anton20@gmail.com>
2024-09-22 11:31:36 +0800

    feat: update ui

--- src/pages/aghniyaIzzul/components/quote/Quote.tsx
+++ src/pages/aghniyaIzzul/components/quote/Quote.tsx
@@ -6,16 +6,16 @@ function Quote() {
     <section className='aghniya__quote'>
         <div className="agniya__quote-cover">
             <div className="agniya__quote-content">
-                {/* <img src={quote} alt="quote" />
+                {/* <img src={quote} alt="quote" /> */}
 
-                <p className='agniya__quote--du-a'>
+                {/* <p className='agniya__quote--du-a'>
                   wa min âyâtihî an khalaqa lakum min 
                   anfusikum azwâjal litaskunû ilaihâ wa ja‘ala 
                   bainakum mawaddataw wa rahmah, inna fî dzâlika 
                   la'âyâtil liqaumiy yatafakkarûn
-                </p>
+                </p> */}
 
-                <p className='agniya__quote--translate'>
+                {/* <p className='agniya__quote--translate'>
                   Di antara tanda-tanda (kebesaran)-Nya ialah 
                   bahwa Dia menciptakan pasangan-pasangan untukmu 
                   dari (jenis) dirimu sendiri agar kamu merasa 
@@ -23,9 +23,9 @@ function Quote() {
                   rasa cinta dan kasih sayang. Sesungguhnya pada 
                   yang demikian itu benar-benar terdapat tanda-tanda 
                   (kebesaran Allah) bagi kaum yang berpikir.
-                </p>
+                </p> */}
 
-                <p className='agniya__quote--surah'>
+                {/* <p className='agniya__quote--surah'>
                   (Ar-Rum · Ayat 21)
                 </p> */}
             </div>
