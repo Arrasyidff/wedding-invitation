465d7470c3e157710b19c9f774ef84fc5e9e481a
Arrasyid Fadel Fatonsyah <aff.anton20@gmail.com>
2024-09-29 09:56:15 +0800

    feat: handle ui cover

--- src/pages/aghniyaIzzul/components/quote/Quote.tsx
+++ src/pages/aghniyaIzzul/components/quote/Quote.tsx
@@ -1,35 +1,38 @@
 import './quote.scss'
-// import quote from '@assets/images/quote.png'
+import quote from '@assets/images/quote.png'
+// import brideImage from '@assets/images/bride-image.png'
+import backgroundImage from '@assets/images/main-background.png'
 
 function Quote() {
   return (
     <section className='agniya__quote'>
-        <div className="agniya__quote-cover">
-            <div className="agniya__quote-content">
-                {/* <img src={quote} alt="quote" /> */}
+      <img className='agniya__quote-background' src={backgroundImage} alt="" />
+      <div className="agniya__quote-cover">
+          <div className="agniya__quote-content">
+              <img src={quote} alt="quote" />
 
-                {/* <p className='agniya__quote--du-a'>
-                  wa min âyâtihî an khalaqa lakum min 
-                  anfusikum azwâjal litaskunû ilaihâ wa ja‘ala 
-                  bainakum mawaddataw wa rahmah, inna fî dzâlika 
-                  la'âyâtil liqaumiy yatafakkarûn
-                </p> */}
+              <p className='agniya__quote--du-a'>
+                wa min âyâtihî an khalaqa lakum min 
+                anfusikum azwâjal litaskunû ilaihâ wa ja‘ala 
+                bainakum mawaddataw wa rahmah, inna fî dzâlika 
+                la'âyâtil liqaumiy yatafakkarûn
+              </p>
 
-                {/* <p className='agniya__quote--translate'>
-                  Di antara tanda-tanda (kebesaran)-Nya ialah 
-                  bahwa Dia menciptakan pasangan-pasangan untukmu 
-                  dari (jenis) dirimu sendiri agar kamu merasa 
-                  tenteram kepadanya. Dia menjadikan di antaramu 
-                  rasa cinta dan kasih sayang. Sesungguhnya pada 
-                  yang demikian itu benar-benar terdapat tanda-tanda 
-                  (kebesaran Allah) bagi kaum yang berpikir.
-                </p> */}
+              <p className='agniya__quote--translate'>
+                Di antara tanda-tanda (kebesaran)-Nya ialah 
+                bahwa Dia menciptakan pasangan-pasangan untukmu 
+                dari (jenis) dirimu sendiri agar kamu merasa 
+                tenteram kepadanya. Dia menjadikan di antaramu 
+                rasa cinta dan kasih sayang. Sesungguhnya pada 
+                yang demikian itu benar-benar terdapat tanda-tanda 
+                (kebesaran Allah) bagi kaum yang berpikir.
+              </p>
 
-                {/* <p className='agniya__quote--surah'>
-                  (Ar-Rum · Ayat 21)
-                </p> */}
-            </div>
-        </div>
+              <p className='agniya__quote--surah'>
+                (Ar-Rum · Ayat 21)
+              </p>
+          </div>
+      </div>
     </section>
   )
 }
