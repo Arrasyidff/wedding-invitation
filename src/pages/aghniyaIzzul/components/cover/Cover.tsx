a262fd087018e9765a654bbf66923f08af7e8bdc
Arrasyid Fadel Fatonsyah <aff.anton20@gmail.com>
2024-09-29 10:45:08 +0800

    fix: update ui cover

--- src/pages/aghniyaIzzul/components/cover/Cover.tsx
+++ src/pages/aghniyaIzzul/components/cover/Cover.tsx
@@ -1,28 +1,30 @@
 import './cover.scss'
 import coverImg from '@assets/images/cover-img.png'
+import coverBackground from '@assets/images/cover-background.png'
 
 function Cover() {
   return (
     <section className='agniya__cover'>
-        <div className="agniya__cover-header">
-            <h2>SAVE THE DATE</h2>
-            <p>FOR THE WEDDING OF</p>
-        </div>
+      <img className='agniya--cover-background' src={coverBackground} alt="" />
+      <div className="agniya__cover-header">
+          <h2>SAVE THE DATE</h2>
+          <p>FOR THE WEDDING OF</p>
+      </div>
 
-        <div className='agniya__cover-content'>
-          <img src={coverImg} alt="cover-image" />
-        </div>
+      <div className='agniya__cover-content'>
+        <img src={coverImg} alt="cover-image" />
+      </div>
 
-        <div className='agniya__cover-invitation'>
-            <span>KEPADA YTH.</span>
-            <span>BAPAK/IBU/SAUDARA/I</span>
-            <h1>BPK. ANDRE</h1>
-            <span>DI TEMPAT</span>
-        </div>
+      <div className='agniya__cover-invitation'>
+          <span>KEPADA YTH.</span>
+          <span>BAPAK/IBU/SAUDARA/I</span>
+          <h1>BPK. ANDRE</h1>
+          <span>DI TEMPAT</span>
+      </div>
 
-        <button className='agniya__cover-open'>
-            BUKA UNDANGAN
-        </button>
+      <button className='agniya__cover-open'>
+          BUKA UNDANGAN
+      </button>
     </section>
   )
 }
